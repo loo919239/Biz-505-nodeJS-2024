81198e6928a6a11aa4f6ef12a3755508a4417a60
loo919239 <loo919239@naver.com>
2024-01-17 15:10:54 +0900

    학생정보v2

--- node_020_student_v2/config/mysql_sample.js
+++ node_020_student_v2/config/mysql_sample.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
 /**
  * 사용하는 MySQL 의 detabese, user, passwerd 를 입력한 후
- * 이 파일을(mysql_sample.js)를 mysql.js로 이름을 변경한 후
+ * 이 파일을(mysql_sample.js)를 mysql.js로 복사한 후
  * 프로젝트를 실행하시오
  */
 const mysql_info = {
