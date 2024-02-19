8dd207b83d1bddc5e09fdd61ae747cb86d9c8ccd
loo919239 <loo919239@naver.com>
2024-02-19 16:21:45 +0900

    메모 만들기

--- node_910_memo/routes/index.js
+++ node_910_memo/routes/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import moment from "moment";
 import DB from "../models/index.js";
+import { upLoad } from "../modules/fileUpload.js";
+
 const MEMOS = DB.models.tbl_memos;
 const router = express.Router();
 
@@ -9,19 +11,47 @@ router.get("/", async (req, res, next) => {
   // moment 를 사용하여 현재 날짜와 시간을 문자열로 getter
   const toDate = moment().format("YYYY-MM-DD");
   const toTime = moment().format("HH:mm:ss");
-
-  //index.pug 를 rendering 할때 사용하도록 보내주기
-  res.render("index", { toDate, toTime });
-});
-
-router.post("/", async (req, res) => {
   try {
-    req.body.m_author = "loo919239@naver.com";
-    await MEMOS.create(req.body);
-    return res.redirect("/");
+    const rows = await MEMOS.findAll();
+    return res.render("index", { MEMOS: rows, toDate, toTime });
   } catch (error) {
     return res.json(error);
   }
+  //index.pug 를 rendering 할때 사용하도록 보내주기
+  // res.render("index", { toDate, toTime });
+});
+
+router.post("/", upLoad.single("m_image"), async (req, res) => {
+  const imageFile = req.file;
+  const m_seq = req.query.seq;
+  // try {
+  req.body.m_image = imageFile?.filename;
+  req.body.m_author = "loo919239@naver.com";
+  if (m_seq) {
+    await MEMOS.update(req.body, { where: { m_seq } });
+  } else {
+    await MEMOS.create(req.body);
+  }
+  return res.redirect("/");
+  // } catch (error) {
+  //   return res.json(error);
+  // }
+});
+
+router.post("/update/:seq", upLoad.single("m_image"), async (req, res) => {
+  const seq = req.params.seq;
+  const imageFile = req.file;
+  req.body.m_image = imageFile?.filename;
+  req.body.m_author = "loo919239@naver.com";
+
+  await MEMOS.update(req.body, { where: { m_seq: seq } });
+  return res.redirect("/");
+});
+
+router.get("/:seq/get", async (req, res) => {
+  const seq = req.params.seq;
+  const row = await MEMOS.findByPk(seq);
+  return res.json(row);
 });
 
 router.get("/get_new_date", async (req, res) => {
